ae19959daca4a43b24da1a3ed35cb3705c193a0d
João Costa <joaolrc@Joaos-MBP.Home>
2022-05-01 22:56:33 +0100

    uptd delay to 5s

--- mouse.py
+++ mouse.py
@@ -3,8 +3,8 @@ import pyautogui, time
 while True:
     print(".")
     pyautogui.moveTo(100,200)
-    time.sleep(2)
+    time.sleep(5)
     pyautogui.moveTo(200,500)
     print("-")
-    time.sleep(2)
+    time.sleep(5)
     
